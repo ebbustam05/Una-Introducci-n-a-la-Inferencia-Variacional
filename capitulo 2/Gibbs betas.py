a84586b28dbe5fee895c93d4d5f447028187b857
ebbustam05 <87913393+ebbustam05@users.noreply.github.com>
2021-09-17 18:17:49 -0500

    Update Gibbs betas.py

--- capitulo 2/Gibbs betas.py
+++ capitulo 2/Gibbs betas.py
@@ -11,9 +11,10 @@ from statsmodels.graphics.tsaplots import plot_acf
 
 tim1=time.clock()
 
-n_a=40 # número de observaciones provenientes de theta A
-n_b=40 # número de observaciones provenientes de theta B
-np.random.seed(345)
+
+n_a=100 # número de observaciones provenientes de theta A
+n_b=100 # número de observaciones provenientes de theta B
+np.random.seed(7259)
 theta=np.array([0.7,0.66])
 x=np.random.binomial(1,theta[0],size=n_a) # observaciones provenientes de theta A
 y=np.random.binomial(1,theta[1],size=n_b) # observaciones provenientes de theta B
@@ -80,16 +81,18 @@ inicial = np.array([0.1,0.05]) #punto inicial
 cadena = np.column_stack((np.zeros(2),inicial))
 lim = 100 # tamaño de muestra
 cont = 0
-probs = np.array([0.5,0.5]) #probabilidad para cada propuesta
 logver = np.array([])
 lag = 10
 muestra = np.zeros((2,1)) # arreglo para guardar simulaciones
 muest = 0 #contador de muestras que se han simulado
 rech = 0 # contador de rechazos
 burn_in = 10
+indicador =0
 
 while muest<lim:
-  indicador = np.random.choice(range(1,3),p=probs) # se elige distribución propuesta
+  indicador = indicador + 1 # se elige distribución propuesta
+  if indicador == 3:
+    indicador = 1
   sh=np.shape(cadena) 
   prop = sim_kernel(cadena[:,sh[1]-1],indicador) # se obtiene propuesta
   aux = rho(cadena[:,sh[1]-1],prop,indicador)
@@ -136,6 +139,16 @@ for i in np.linspace(ainf,asup,parta):
       b=b+1
   a=a+1
 
+media_sim = np.mean(muestra,axis=1)
+
+print("Segundos: ", timf,sep="")
+print("Minutos: ", timf_m,sep="")
+print("")
+print("Iteraciones: ", cont,sep="")
+print("")
+print('Estimador puntual theta A:',np.round(media_sim[0],4),sep="")
+print('Estimador puntual theta B:',np.round(media_sim[1],4),sep="")
+
 menos = 50
 plt.contourf(np.linspace(ainf,asup,parta),np.linspace(binf,bsup,partb),np.transpose(Z),levels=50)
 plt.colorbar().ax.set_ylabel('densidad no normalizada')
@@ -159,7 +172,7 @@ fig.text(0.04, 0.5, 'Autocorrelación', va='center', rotation='vertical')
 plt.show()
 
 
-media_sim = np.mean(muestra,axis=1)
+
 
 
 
@@ -175,18 +188,16 @@ plt.show()
 l1=np.linspace(0,1,num=10)
 plt.contourf(np.linspace(ainf,asup,parta),np.linspace(binf,bsup,partb),np.transpose(Z),levels=50)
 plt.colorbar().ax.set_ylabel('densidad no normalizada')
-plt.scatter(muestra[0,1:lim+1],muestra[1,1:lim+1],marker='o',color="orange",facecolors='none',label='Muestra',linewidth=1.5)
-plt.legend()
-#plt.scatter(media_sim[0],media_sim[1],facecolors="r")
+#plt.scatter(muestra[0,1:lim+1],muestra[1,1:lim+1],marker='o',color="orange",facecolors='none',label='Muestra',linewidth=1.0)
+#plt.legend()
+plt.scatter(media_sim[0],media_sim[1],facecolors="r")
 #plt.plot(l1,l1,color="r")
 plt.show()
 
-
-
-print("Segundos: ", timf,sep="")
-print("Minutos: ", timf_m,sep="")
-print("")
-print("Iteraciones: ", cont,sep="")
-print("")
-print('Estimador puntual theta A:',np.round(media_sim[0],4),sep="")
-print('Estimador puntual theta B:',np.round(media_sim[1],4),sep="")
+plt.contourf(np.linspace(ainf,asup,parta),np.linspace(binf,bsup,partb),np.transpose(Z),levels=50)
+plt.colorbar().ax.set_ylabel('densidad no normalizada')
+plt.scatter(muestra[0,1:lim+1],muestra[1,1:lim+1],marker='.',color="orange",facecolors='none',label='Muestra',linewidth=0.5)
+plt.legend(markerscale=2,facecolor='white')
+#plt.scatter(media_sim[0],media_sim[1],facecolors="r")
+#plt.plot(l1,l1,color="r")
+plt.show()
